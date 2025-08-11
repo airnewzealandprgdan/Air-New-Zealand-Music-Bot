42bfe917c8a8ad27fe4b7a664bb45b707ea7265b
airnewzealandprgdan <ptfsairnewzealand@gmail.com>
2025-08-12 02:51:24 +0300

    Create packages.js

---
+++ packages.js
@@ -0,0 +1,20 @@
+{
+  "name": "air-new-zealand-music-bot",
+  "version": "1.0.0",
+  "description": "Discord music bot for Air New Zealand",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@discordjs/voice": "^0.14.0",
+    "@distube/ytdl-core": "^1.0.10",
+    "@types/node": "^20.2.5",
+    "discord.js": "^14.11.0",
+    "yt-dlp-wrap": "^1.3.5",
+    "ytdl-core": "^4.11.1"
+  },
+  "engines": {
+    "node": ">=16.9.0"
+  }
+}
